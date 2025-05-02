bae1ce98a1cf5855b2f924a8692eeac84e185f0d
Isfan <isfanbimantara@gmail.com>
2025-05-02 10:03:11 +0700

    final commit

--- script.js
+++ script.js
@@ -4,23 +4,39 @@ let year = newDate.getFullYear();
 let getFullYear = document.getElementById('getFullYear')
 getFullYear.innerHTML = year;
 
-let btnForShop = document.getElementById('btnForShop')
-btnForShop.innerHTML = '$455'
+let miniConsole = document.getElementById('miniConsole')
+let videoConsole = document.getElementById('videoConsole')
+miniConsole.innerHTML = '$455'
+videoConsole.innerHTML = '$123'
 
 let isBtn = document.createElement('button')
 isBtn.className = 'shopBtn'
 isBtn.innerHTML = 'SHOP NOW'
 
 function functionEnter(e) {
-    if(btnForShop.className.includes('avail')){
-        btnForShop.innerHTML = ''
-        btnForShop.append(isBtn)
+    if(e == 'mini') {
+        if(miniConsole.className.includes('avail')){
+            miniConsole.innerHTML = ''
+            miniConsole.append(isBtn)
+        }
+    }else if(e ==  'video'){
+        if(videoConsole.className.includes('avail')){
+            videoConsole.innerHTML = ''
+            videoConsole.append(isBtn)
+        }
     }
 }
 function functionLeave(e) {
-    if(btnForShop.className.includes('avail')){
-        btnForShop.style.display = 'block'
-        btnForShop.innerHTML = '$455'
+    if(e == 'mini') {
+        if(miniConsole.className.includes('avail')){
+            miniConsole.style.display = 'block'
+            miniConsole.innerHTML = '$455'
+        }
+    }else if(e == 'video'){
+        if(videoConsole.className.includes('avail')){
+            videoConsole.style.display = 'block'
+            videoConsole.innerHTML = '$123'
+        }
     }
 }
 
